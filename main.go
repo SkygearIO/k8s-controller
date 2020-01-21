f19abde3f82d036f018efaba53ca34d8be7070f9
kiootic <kiootic@oursky.com>
2020-01-21 14:16:25 +0800

    Add custom domain CRD

--- main.go
+++ main.go
@@ -76,6 +76,14 @@ func main() {
 		setupLog.Error(err, "unable to create webhook", "webhook", "CustomDomainRegistration")
 		os.Exit(1)
 	}
+	if err = (&controllers.CustomDomainReconciler{
+		Client: mgr.GetClient(),
+		Log:    ctrl.Log.WithName("controllers").WithName("CustomDomain"),
+		Scheme: mgr.GetScheme(),
+	}).SetupWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create controller", "controller", "CustomDomain")
+		os.Exit(1)
+	}
 	// +kubebuilder:scaffold:builder
 
 	setupLog.Info("starting manager")
